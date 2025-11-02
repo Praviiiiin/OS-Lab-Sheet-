0a1f9eb4c283bb30a03af88d94778513e5cbfc91
E Pravin <162611077+Praviiiiin@users.noreply.github.com>
2025-11-02 13:51:47 +0530

    Create subtask3.py

---
+++ Lab Sheet 2/subtask3.py
@@ -0,0 +1,12 @@
+import multiprocessing
+from subtask2 import system_process
+if __name__ == '__main__':
+	print("System Starting...")
+	p1 = multiprocessing.Process(target=system_process, args=('Process-1',))
+	p2 = multiprocessing.Process(target=system_process, args=('Process-2',))
+	p1.start()
+	p2.start()
+
+	p1.join()
+	p2.join()
+	print("System Shutdown.")
