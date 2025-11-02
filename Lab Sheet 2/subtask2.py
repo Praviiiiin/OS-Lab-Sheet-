8ad5a84f853f967d27ec043715f517fc2ae267a6
E Pravin <162611077+Praviiiiin@users.noreply.github.com>
2025-11-02 13:55:04 +0530

    Update subtask2.py

--- Lab Sheet 2/subtask2.py
+++ Lab Sheet 2/subtask2.py
@@ -1,6 +1,13 @@
-import logging 
 import time
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s',
+    datefmt='%H:%M:%S'
+)
 def system_process(task_name):
-	logging.info(f"{task_name} started")
-	time.sleep(2)  # Simulate task delay
-	logging.info(f"{task_name} ended")
+    """Simulates a small system process task."""
+    logging.info(f"{task_name} started")
+    time.sleep(2)  
+    logging.info(f"{task_name} ended")
+system_process("Process-1")
